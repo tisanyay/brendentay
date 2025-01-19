6d7e8a53add1cbab385aeda0dd31f2611291902a
Yisan Tay <brendentay@meridian.jwong.dev>
2025-01-19 21:26:36 +0800

    minor correction

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
         <h2>Introduction</h2>
           <div className="item">
             <p>I graduated from Singapore Polytechnic with a diploma in Applied AI and Analytics. I learnt many data science skills such as data analysis, machine learning and artificial intelligence, data engineering, web development, and cloud computing. In the course, I used Python to work with datasets, build AI models, produce visualisations using libraries such as matplotlib, build machine learning web applications, etc.</p>
-            <p>I am currently an National Serviceman (NSF) that is passionate about learning new things and trying out new hobbies. I enlisted in July of last year and is set to ORD in 3 May 2026, which means I'm serving for 1 year 10 months. I had the opportunity to meet lots of new people and developed my interpersonal skills which allows me to work better in a team. When activated during duty, I have to work in a squad of 7 soldiers and make quick decisions in a high-pressure situation which trains my quick decision-making skills and adaptability.</p>
+            <p>I am currently an National Serviceman (NSF) that is passionate about learning new things and trying out new hobbies. I enlisted in July of last year and is set to ORD in 3 May 2026, which means I'm serving for 1 year 10 months. I had the opportunity to meet lots of new people and developed my interpersonal skills which allows me to work better in a team. When activated during duty, I have to work in a squad of 7 soldiers and make quick decisions in a high-pressure situation which trains my decision-making skills and adaptability.</p>
           </div>
       </section>
       <section>

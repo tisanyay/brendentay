2f1e344ffa26f1fe707153961ad84bdaf68ce3e7
Yisan Tay <brendentay@meridian.jwong.dev>
2025-01-23 15:28:40 +0800

    added content

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,13 +7,13 @@ export default function AboutMe() {
         <h2>Introduction</h2>
           <div className="item">
             <p>I graduated from Singapore Polytechnic with a diploma in Applied AI and Analytics. I learnt many data science skills such as data analysis, machine learning and artificial intelligence, data engineering, web development, and cloud computing. In the course, I used Python to work with datasets, build AI models, produce visualisations using libraries such as matplotlib, build machine learning web applications, etc.</p>
-            <p>I am currently an National Serviceman (NSF) that is passionate about learning new things and trying out new hobbies. I enlisted in July of last year and is set to ORD on 3 May 2026, which means I'm serving for 1 year 10 months. I had the opportunity to meet lots of new people and developed my interpersonal skills which allows me to work better in a team. When activated during duty, I have to work in a squad of 7 soldiers and make quick decisions in a high-pressure situation which trains my decision-making skills and adaptability.</p>
+            <p>I am currently an NSF that enlisted in July of last year and is set to ORD on 3 May 2026. I had the opportunity to meet lots of new personalities and developed my interpersonal skills which allows me to work better in a team. As a security trooper, I serve in the Immediate Response Force to stop any intrusions within the air base. When activated during duty, I have to work in a squad of 7 soldiers and make quick decisions in a high-pressure environment which trains my decision-making skills and adaptability.</p>
           </div>
       </section>
       <section>
         <h2>Hobbies</h2>
           <div className="item">
-            <p>My hobbies are playing the bass and classical guitar, gaming, basketball, exercising, and crocheting. I am excited to learn more fun skills such as sewing, cooking, and photography.</p>
+            <p>To unwind, I dabble in many hobbies. Recently, I have gotten into playing the classical guitar for bossa nova and learning music theory. I was inspired to do this after my friend lent me his bass guitar that I used to play metal for a few months. I aim to be able to create my first bossa nova piece. Besides music, I play video games like Elden Ring and Counter-strike since I am a huge fan of first-person shooter and RPG games. Since I enlisted, I have had more free time at home to try out new things such as crocheting, cooking, and building Gundam toys. I plan to learn sewing and photography over the course of my NS life.</p>
           </div>
       </section>
     </div>

0aad5bb3383b51105f1b8ae500abcfa85831c251
Yisan Tay <brendentay@meridian.jwong.dev>
2025-02-02 18:44:28 +0800

    added videos

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -10,12 +10,22 @@ export default function AboutMe() {
             <p>I am currently an NSF that enlisted in July of last year and is set to ORD on 3 May 2026. I had the opportunity to meet lots of new personalities and developed my interpersonal skills which allows me to work better in a team. As a security trooper, I serve in the Immediate Response Force to stop any intrusions within the air base. When activated during duty, I have to work in a squad of 7 soldiers and make quick decisions in a high-pressure environment which trains my decision-making skills and adaptability.</p>
           </div>
       </section>
+
       <section>
         <h2>Hobbies</h2>
           <div className="item">
             <p>To unwind, I dabble in many hobbies. Recently, I have gotten into playing the classical guitar for bossa nova and learning music theory. I was inspired to do this after my friend lent me his bass guitar that I used to play metal for a few months. I aim to be able to create my first bossa nova piece. Besides music, I play video games like Elden Ring and Counter-strike since I am a huge fan of first-person shooter and RPG games. Since I enlisted, I have had more free time at home to try out new things such as crocheting, cooking, and building Gundam toys. I plan to learn sewing and photography over the course of my NS life.</p>
           </div>
       </section>
+
+      <section>
+        <h3>Bass covers</h3>
+        <div className="video-row">
+          <iframe width="420" height="315" src="https://youtube.com/embed/DtXQyvkHg9c"></iframe>
+          <iframe width="420" height="315" src="https://youtube.com/embed/xhpgSO5uco0"></iframe>
+        </div>
+      </section>
+
     </div>
   )
 }
